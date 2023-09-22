22f70cc795cfde5961659c775bb091a235a205f4
Filoppi <filippotarpini@hotmail.it>
2023-09-22 21:03:21 +0300

    First version of DICE like tonemapper

--- shaders/math.h
+++ shaders/math.h
@@ -2,6 +2,9 @@
 #define MATH_H
 
 #define FLT_MAX 3.402823466e+38F
+#define FLT10_MAX 64512.f
+#define FLT11_MAX 65024.f
+#define FLT16_MAX 65504.f
 
 float hypot3(float3 input)
 {
@@ -14,6 +17,41 @@ template<class T>
 T linearNormalization(T input, float min, float max, float newMin, float newMax)
 {
 	return ((input - min) * ((newMax - newMin) / (max - min))) + newMin;
+}
+
+// Aplies exponential ("Photographic") luma compression.
+// The pow can modulate the curve without changing the values around the edges.
+float rangeCompressPow(float x, float fMax = FLT_MAX, float fPow = 1.f)
+{
+	// Branches are for static parameters optimizations
+	if (fPow == 1.f && fMax == FLT_MAX)
+	{
+		// This does e^x. We expect x to be between 0 and 1.
+		return 1.f - exp(-x);
+	}
+	if (fPow == 1.f && fMax != FLT_MAX)
+	{
+		const float fLostRange = exp(-fMax);
+		const float fRestoreRangeScale = 1.f / (1.f - fLostRange);
+		return (1.f - exp(-x)) * fRestoreRangeScale;
+	}
+	if (fPow != 1.f && fMax == FLT_MAX)
+	{
+		return (1.f - pow(exp(-x), fPow));
+	}
+	const float fLostRange = pow(exp(-fMax), fPow);
+	const float fRestoreRangeScale = 1.f / (1.f - fLostRange);
+	return (1.f - pow(exp(-x), fPow)) * fRestoreRangeScale;
+}
+
+// Refurbished DICE HDR tonemapper
+float luminanceCompress(float fInValue, float fOutMaxValue, float fShoulderStart = 0.f, float fInMaxValue = FLT_MAX, float fModulationPow = 1.f)
+{
+	float fCompressableValue = fInValue - fShoulderStart;
+	float fCompressableRange = fInMaxValue - fShoulderStart;
+	float fCompressedRange = fOutMaxValue - fShoulderStart;
+	float fPossibleOutValue = fShoulderStart + fCompressedRange * rangeCompressPow(fCompressableValue / fCompressedRange, fCompressableRange / fCompressedRange, fModulationPow);
+	return fInValue <= fShoulderStart ? fInValue : fPossibleOutValue;
 }
 
 #endif
