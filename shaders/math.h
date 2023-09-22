16eaccf06e63e3a18bd3566a27c80a5ff167d53c
Filoppi <filippotarpini@hotmail.it>
2023-09-22 03:21:36 +0300

    Add normalized LUTs

--- shaders/math.h
+++ shaders/math.h
@@ -3,4 +3,17 @@
 
 #define FLT_MAX 3.402823466e+38F
 
+float hypot3(float3 input)
+{
+	float3 inputSquared = input * input;
+	return sqrt(inputSquared.x + inputSquared.y + inputSquared.z);
+}
+
+// From old to new range
+template<class T>
+T linearNormalization(T input, float min, float max, float newMin, float newMax)
+{
+	return ((input - min) * ((newMax - newMin) / (max - min))) + newMin;
+}
+
 #endif
