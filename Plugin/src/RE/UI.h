8b451aed7844b4fbace2ff9d56f75eb5541f049f
Nukem <Nukem@outlook.com>
2023-09-24 09:31:56 -0400

    BSFixedStringCS belongs in its own file

--- Plugin/src/RE/UI.h
+++ Plugin/src/RE/UI.h
@@ -1,32 +1,10 @@
 #pragma once
 
+#include "BSFixedString.h"
 #include "UIDataShuttle.h"
 
 namespace RE
 {
-	class BSFixedStringCS
-	{
-	private:
-		void* m_EntryPoolData = nullptr;
-
-	public:
-		BSFixedStringCS() = default;
-		BSFixedStringCS(const char *String)
-		{
-			auto addr = dku::Hook::Module::get().base() + 0x3149530;
-			auto func = reinterpret_cast<void (*)(BSFixedStringCS*, const char *)>(addr);
-			func(this, String);
-		}
-		BSFixedStringCS(const BSFixedStringCS& Other) = delete;
-		~BSFixedStringCS()
-		{
-			auto addr = dku::Hook::Module::get().base() + 0x0546D20;
-			auto func = reinterpret_cast<void (*)(BSFixedStringCS*)>(addr);
-			func(this);
-		}
-		BSFixedStringCS& operator=(const BSFixedStringCS& Other) = delete;
-	};
-
 	class __declspec(novtable) IUIValue
 	{
 	public:
