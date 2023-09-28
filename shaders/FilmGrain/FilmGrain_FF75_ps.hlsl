eef7587c2b547cdc94067993e708236481e1f654
Carlos Lopez <clshortfuse@gmail.com>
2023-09-28 08:00:47 -0400

    docs(flmgrain): add reverse engineer notes

--- shaders/FilmGrain/FilmGrain_FF75_ps.hlsl
+++ shaders/FilmGrain/FilmGrain_FF75_ps.hlsl
@@ -24,8 +24,28 @@ struct SPIRV_Cross_Output
     float4 SV_Target : SV_Target0;
 };
 
+// Better? random generator
+float rand(float2 uv) {
+    return frac(sin(dot(uv,float2(12.9898,78.233)))*43758.5453123);
+}
+
+
+// Returns a random number between 0 and 1 based on seed
+float bethesdaRandom(float seed) {
+    return frac(sin(seed) * 493013.f);
+}
+
+
 void frag_main()
 {
+// TEXCOORD = float2 [0,1] relative to screen
+// TonemappedColorTexture is the framebuffer?
+// Generate random noise and overlay on top of screen buffer
+// filmGrainColorAndIntensity
+//   .x - randomInt32? changed every frame?
+//   .y = randomInt32? changed every frame?
+//   .z = strength? maybe 0.03
+
     float3 tonemappedColor = TonemappedColorTexture.Sample(Sampler0, float2(TEXCOORD.x, TEXCOORD.y));
 #if ENABLE_HDR
     tonemappedColor /= HDR_GAME_PAPER_WHITE;
@@ -42,13 +62,51 @@ void frag_main()
     const float filmGrainInvSize = 1.f / (FILM_GRAIN_TEXTURE_SIZE - 1u);
     const float filmGrainHalfSize = 521.f; //TODO: rename in case we keep this as 521
 #endif
-    float additiveFilmGrain = (((frac(sin(((float(int(uint(filmGrainColorAndIntensity.x) & (FILM_GRAIN_TEXTURE_SIZE - 1u))) * filmGrainInvSize) + TEXCOORD.x) + (((float(int(uint(filmGrainColorAndIntensity.y) & (FILM_GRAIN_TEXTURE_SIZE - 1u))) * filmGrainInvSize) + TEXCOORD.y) * filmGrainHalfSize)) * 493013.f) * 2.f) - 1.f) * filmGrainColorAndIntensity.z) * inverseLuminance;
-    float3 tonemappedColorWithFilmGrain = tonemappedColor + float3(additiveFilmGrain, additiveFilmGrain, additiveFilmGrain); // Note: we let this possibly generate colors below 1 for scRGB
+    // Applying modulus against 1023 will give value between 0 and 1023
+    // Same two values for all texels this frame
+    float randomFromRange1 = float(int(uint(filmGrainColorAndIntensity.x) & (FILM_GRAIN_TEXTURE_SIZE - 1u)));
+    float randomFromRange2 = float(int(uint(filmGrainColorAndIntensity.y) & (FILM_GRAIN_TEXTURE_SIZE - 1u)));
+    
+    // Divide by 1023 to get a number between 0 and 1
+    float randomNormalized1 = randomFromRange1 * filmGrainInvSize;
+    float randomNormalized2 = randomFromRange1 * filmGrainInvSize;
+    
+    // Offset by x/y position
+    float uniqueX = randomNormalized1 + TEXCOORD.x;
+    float uniqueY = (randomNormalized2 + TEXCOORD.y) * filmGrainHalfSize;
+    
+    float seed = (uniqueX + uniqueY);
+    // Random number between -1 and -1;
+    // luminanceShift at 0 is black, 0.5 unchanged, 1 negative of current texel
+    float luminanceShift = (bethesdaRandom(seed) * 2.f) - 1.f;
+
+#if 0 // WIP fixes
+    float additiveFilmGrain = (luminanceShift * 1.f * inverseLuminance);
+    float3 tonemappedColorWithFilmGrain = float3(additiveFilmGrain, additiveFilmGrain,additiveFilmGrain);
+#else
+    float additiveFilmGrain = (luminanceShift * filmGrainColorAndIntensity.z * inverseLuminance);
+    float3 newColor = float3(additiveFilmGrain, additiveFilmGrain,additiveFilmGrain);
+    
+    // Use addition to overlay color on top
+    // Note: we let this possibly generate colors below 1 for scRGB
+    float3 tonemappedColorWithFilmGrain = tonemappedColor + newColor;
+#endif
+
 #if ENABLE_HDR
     tonemappedColorWithFilmGrain = gamma_sRGB_to_linear(tonemappedColorWithFilmGrain) * HDR_GAME_PAPER_WHITE;
 #endif // ENABLE_HDR
 
-    SV_Target.xyz = tonemappedColorWithFilmGrain;
+#if 0 // WIP fixes
+    // Right side only
+    if (TEXCOORD.x >= 0.5f) {
+        SV_Target.rgb = tonemappedColorWithFilmGrain;
+    } else {
+        SV_Target.rgb = tonemappedColor;
+    }
+#else
+    SV_Target.rgb = tonemappedColorWithFilmGrain;
+#endif
+
     SV_Target.w = 1.0f;
 }
 
