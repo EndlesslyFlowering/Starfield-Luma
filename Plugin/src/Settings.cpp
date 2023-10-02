4d80fb21d6dd6333d62fae69feb92ade37f3c75f
ersh1 <16157893+ersh1@users.noreply.github.com>
2023-10-03 01:50:23 +0200

    refactored settings; sdr's swapchain is now RGB10A2

--- Plugin/src/Settings.cpp
+++ Plugin/src/Settings.cpp
@@ -2,66 +2,86 @@
 
 namespace Settings
 {
+    float Slider::GetSliderPercentage() const
+    {
+		return (value.get_data() - sliderMin) / (sliderMax - sliderMin);
+    }
+
+    std::string Slider::GetSliderText() const
+    {
+		return std::format("{}", value.get_data());
+    }
+
+    float Slider::GetValueFromSlider(float a_percentage) const
+    {
+		return std::roundf(a_percentage * (sliderMax - sliderMin) + sliderMin);
+    }
+
+    void Slider::SetValueFromSlider(float a_percentage)
+    {
+        *value = GetValueFromSlider(a_percentage);
+    }
+
     bool Main::IsHDREnabled() const
     {
-		return FrameBufferFormat.get_data() != 0;
+		return DisplayMode.value.get_data() != 0;
     }
 
-    float Main::GetMaxLuminanceSliderPercentage() const
+    RE::BS_DXGI_FORMAT Main::GetDisplayModeFormat() const
     {
-		return (MaxLuminance.get_data() - luminanceSliderMin) / (luminanceSliderMax - luminanceSliderMin);
+		switch (DisplayMode.value.get_data()) {
+		case 0:
+		case 1:
+		default:
+			return RE::BS_DXGI_FORMAT::BS_DXGI_FORMAT_R10G10B10A2_UNORM;
+		case 2:
+			return RE::BS_DXGI_FORMAT::BS_DXGI_FORMAT_R16G16B16A16_FLOAT;
+		}
     }
 
-    std::string Main::GetMaxLuminanceText() const
+    DXGI_COLOR_SPACE_TYPE Main::GetDisplayModeColorSpaceType() const
     {
-		return std::format("{}", MaxLuminance.get_data());
-    }
-
-    float Main::GetMaxLuminanceFromSlider(float a_percentage) const
-    {
-		return std::roundf(a_percentage * (luminanceSliderMax - luminanceSliderMin) + luminanceSliderMin);
-    }
-
-    void Main::SetMaxLuminanceFromSlider(float a_percentage)
-    {
-		*MaxLuminance = GetMaxLuminanceFromSlider(a_percentage);
-    }
-
-    float Main::GetPaperwhiteSliderPercentage() const
-    {
-		return (Paperwhite.get_data() - paperwhiteSliderMin) / (paperwhiteSliderMax - paperwhiteSliderMin);
-    }
-
-    std::string Main::GetPaperwhiteText() const
-    {
-		return std::format("{}", Paperwhite.get_data());
-    }
-
-    float Main::GetPaperwhiteFromSlider(float a_percentage) const
-    {
-		return std::roundf(a_percentage * (paperwhiteSliderMax - paperwhiteSliderMin) + paperwhiteSliderMin);
-    }
-
-    void Main::SetPaperwhiteFromSlider(float a_percentage)
-    {
-		*Paperwhite = GetPaperwhiteFromSlider(a_percentage);
+		switch (DisplayMode.value.get_data()) {
+		case 0:
+		default:
+			return DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709;
+		case 1:
+			return DXGI_COLOR_SPACE_RGB_FULL_G2084_NONE_P2020;
+		case 2:
+			return DXGI_COLOR_SPACE_RGB_FULL_G10_NONE_P709;
+		}
     }
 
     void Main::Load() noexcept
 	{
 		static std::once_flag ConfigInit;
 		std::call_once(ConfigInit, [&]() {
-			config.Bind(ImageSpaceBufferFormat, 0);
-			config.Bind(UpgradeUIRenderTarget, true);
-			config.Bind(UpgradeRenderTargets, 2);
-			config.Bind(FrameBufferFormat, 0);
-			config.Bind(MaxLuminance, 1000.f);
-			config.Bind(Paperwhite, 200.f);
-			config.Bind(RenderTargetsToUpgrade, "SF_ColorBuffer", "HDRImagespaceBuffer", "ImageSpaceHalfResBuffer", "ImageProcessColorTarget", "ImageSpaceBufferB10G11R11", "ImageSpaceBufferE5B9G9R9", "TAA_idTech7HistoryColorTarget", "EnvBRDF", "GBuffer_Normal_EmissiveIntensity", "ImageSpaceBufferR10G10B10A2");
+			config.Bind(DisplayMode.value, 0);
+			config.Bind(PeakBrightness.value, PeakBrightness.defaultValue);
+			config.Bind(GamePaperWhite.value, GamePaperWhite.defaultValue);
+			config.Bind(UIPaperWhite.value, UIPaperWhite.defaultValue);
+			config.Bind(RenderTargetsToUpgrade,
+			    "ImageSpaceBuffer",
+				"ScaleformCompositeBuffer",
+				"SF_ColorBuffer",
+				"HDRImagespaceBuffer",
+				"ImageSpaceHalfResBuffer",
+				"ImageProcessColorTarget",
+				"ImageSpaceBufferB10G11R11",
+				"ImageSpaceBufferE5B9G9R9",
+				"TAA_idTech7HistoryColorTarget",
+				"EnvBRDF",
+				"ImageSpaceBufferR10G10B10A2");
 		});
 
 		config.Load();
 
 		INFO("Config loaded"sv)
 	}
+
+    void Main::Save() noexcept
+    {
+		config.Generate();
+		config.Write();
+    }
 }
