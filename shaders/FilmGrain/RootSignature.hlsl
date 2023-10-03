9bee85eeebfe0ffdbf854855189bce97c63fd476
Nukem <Nukem@outlook.com>
2023-10-02 20:41:49 -0400

    Add shader/pipeline root signatures for later use and modification

---
+++ shaders/FilmGrain/RootSignature.hlsl
@@ -0,0 +1,9 @@
+#define ShaderRootSignature \
+	"RootFlags(ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT), " \
+	"RootConstants(num32BitConstants=3, b0), " \
+	\
+	"DescriptorTable(" \
+		"SRV(t0, space=8) " \
+	"), " \
+	\
+	"StaticSampler(s0, space=8) "
