15d69330767065acc57f790a687b4d6f91e09780
Carlos Lopez <clshortfuse@gmail.com>
2023-10-07 00:00:16 -0400

    feat(filmgrain): add 2 new types

--- shaders/FilmGrain/FilmGrain_ps.hlsl
+++ shaders/FilmGrain/FilmGrain_ps.hlsl
@@ -4,6 +4,9 @@
 
 #define FILM_GRAIN_TEXTURE_SIZE 1024u
 
+// 0 Vanilla, 1 New Random (no flicker/repeating), 2 New random, no raised blacks
+#define FILM_GRAIN_TYPE 0
+
 cbuffer _13_15 : register(b0, space0)
 {
 	float4 filmGrainColorAndIntensity : packoffset(c0);
@@ -25,7 +28,7 @@ struct SPIRV_Cross_Output
 	float4 SV_Target : SV_Target0;
 };
 
-// Better? random generator
+// Better random generator
 float rand(float2 uv) {
 	return frac(sin(dot(uv,float2(12.9898,78.233)))*43758.5453123);
 }
@@ -45,24 +48,18 @@ void frag_main()
 // filmGrainColorAndIntensity
 //   .x - randomInt32? changed every frame?
 //   .y = randomInt32? changed every frame?
-//   .z = strength? maybe 0.03
+//   .z = Film Grading Intensity (0 - 0.03)
 
 	float3 tonemappedColor = TonemappedColorTexture.Sample(Sampler0, float2(TEXCOORD.x, TEXCOORD.y));
 #if ENABLE_HDR
 	tonemappedColor /= HDR_GAME_PAPER_WHITE;
 	tonemappedColor = gamma_linear_to_sRGB(tonemappedColor);
-	float inverseLuminance = saturate(1.f - Luminance(tonemappedColor));
-#else
-	float inverseLuminance = saturate(1.f - Luminance(tonemappedColor));
 #endif // ENABLE_HDR
+float colorY = Luminance(tonemappedColor);
 
-#if 0 // WIP fixes
-	const float filmGrainInvSize = 1.f / FILM_GRAIN_TEXTURE_SIZE; // This was "1.f / (FILM_GRAIN_TEXTURE_SIZE - 1u)", though that seems incorrect
-	const float filmGrainHalfSize = FILM_GRAIN_TEXTURE_SIZE * 0.5f; // This was "521.f", which seems like a mistake on 512
-#else
+#if FILM_GRAIN_TYPE == 0
 	const float filmGrainInvSize = 1.f / (FILM_GRAIN_TEXTURE_SIZE - 1u);
 	const float filmGrainHalfSize = 521.f; //TODO: rename in case we keep this as 521
-#endif
 	// Applying modulus against 1023 will give value between 0 and 1023
 	// Same two values for all texels this frame
 	float randomFromRange1 = float(int(uint(filmGrainColorAndIntensity.x) & (FILM_GRAIN_TEXTURE_SIZE - 1u)));
@@ -79,36 +76,46 @@ void frag_main()
 	// Unique each frame and texel. (Is it though?)
 	float seed = (uniqueX + uniqueY);
 
-	// Generate a random number between -1 and -1;
-	// luminanceShift at -1 is black, 0 unchanged, 1 negative of current texel
-	float luminanceShift = (bethesdaRandom(seed) * 2.f) - 1.f;
-
-#if 0 // WIP fixes
-	float additiveFilmGrain = (luminanceShift * 1.f * inverseLuminance);
-	float3 tonemappedColorWithFilmGrain = float3(additiveFilmGrain, additiveFilmGrain,additiveFilmGrain);
+	// Generate a random number between 0 and 1;
+	float randomNumber = bethesdaRandom(seed);
 #else
+	float randomNumber = rand(TEXCOORD.xy
+		+ frac(filmGrainColorAndIntensity.x / 2147483647.f)
+		+ frac(filmGrainColorAndIntensity.y / 2147483647.f)
+	);
+#endif // FILM_GRAIN_TYPE == 0
+
+#if FILM_GRAIN_TYPE != 2
+	float inverseLuminance = saturate(1.f - colorY);
+	// luminanceShift at -1 is black, 0 unchanged, 1 negative of current texel
+	float luminanceShift = (randomNumber * 2.f) - 1.f;
 	float additiveFilmGrain = (luminanceShift * filmGrainColorAndIntensity.z * inverseLuminance);
+#else
+	float additiveFilmGrain = ((randomNumber * 2.f) - 1.f) // (-1 to 1)
+		* (2.f* (0.5f - abs(colorY - 0.5f))) // Bias towards center (nothing if black or white)
+		* filmGrainColorAndIntensity.z
+		* 3.333f;
+#endif // FILM_GRAIN_TYPE != 2
+
 	float3 newColor = float3(additiveFilmGrain, additiveFilmGrain,additiveFilmGrain);
 
 	// Use addition to overlay color on top
 	// Note: we let this possibly generate colors below 1 for scRGB
 	float3 tonemappedColorWithFilmGrain = tonemappedColor + newColor;
+
+
+#if 0 // WIP fixes
+	// Right side only
+	if (TEXCOORD.x < 0.5f) {
+		tonemappedColorWithFilmGrain = tonemappedColor;
+	}
 #endif
 
 #if ENABLE_HDR
 	tonemappedColorWithFilmGrain = gamma_sRGB_to_linear(tonemappedColorWithFilmGrain) * HDR_GAME_PAPER_WHITE;
 #endif // ENABLE_HDR
 
-#if 0 // WIP fixes
-	// Right side only
-	if (TEXCOORD.x >= 0.5f) {
-		SV_Target.rgb = tonemappedColorWithFilmGrain;
-	} else {
-		SV_Target.rgb = tonemappedColor;
-	}
-#else
 	SV_Target.rgb = tonemappedColorWithFilmGrain;
-#endif
 
 	SV_Target.w = 1.0f;
 }
