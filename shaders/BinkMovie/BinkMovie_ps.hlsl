d253addee3c60d7fa71f096ba903580f7b70042b
Lilium <EndlesslyFlowering@protonmail.com>
2023-10-07 02:15:21 +0200

    fix matrix in BinkMovie which was for BT.601 limited range. correct is BT.709 full range.

--- shaders/BinkMovie/BinkMovie_ps.hlsl
+++ shaders/BinkMovie/BinkMovie_ps.hlsl
@@ -2,10 +2,11 @@
 #include "../color.hlsl"
 #include "RootSignature.hlsl"
 
-Texture2D<float> _8 : register(t0, space8);
-Texture2D<float> _9 : register(t1, space8);
-Texture2D<float> _10 : register(t2, space8);
-SamplerState _13 : register(s0, space8);
+Texture2D<float> TexY  : register(t0, space8);
+Texture2D<float> TexCb : register(t2, space8);
+Texture2D<float> TexCr : register(t1, space8);
+
+SamplerState Sampler0 : register(s0, space8);
 
 struct PSInputs
 {
@@ -16,32 +17,29 @@ struct PSInputs
 [RootSignature(ShaderRootSignature)]
 float4 PS(PSInputs inputs) : SV_Target
 {
-	float Y = _8.Sample(_13, inputs.TEXCOORD.xy).x;
-	float Cb = _9.Sample(_13, inputs.TEXCOORD.xy).x;
-	float Cr = _10.Sample(_13, inputs.TEXCOORD.xy).x;
-	float3 color;
-#if 0 // Doesn't work, the videos were probably not in YCbCr
-	// Reference: https://gist.github.com/yohhoy/dafa5a47dade85d8b40625261af3776a
-	static const float a = 0.2126f;
-	static const float b = 0.7152f;
-	static const float c = 0.0722f;
-	static const float d = 1.8556f;
-	static const float e = 1.5748f;
-	color.r = Y + (Cr * e);
-	color.g = Y - (Cb * (c * d / b)) - (Cr * (a * e / b));
-	color.b = Y + (Cb * d);
-#else // Original formula, it doesn't seem to make much sense but it works
-	//TODO: could it be that by treating Y in a different way, we could extract HDR luminance from this video? Probably not.
-	Y *= 1.16412353515625f;
-	color.x = (Y - 0.870655059814453125f) + (Cb * 1.595794677734375f);
-	color.y = ((Y + 0.529705047607421875f) - (Cb * 0.8134765625f)) - (Cr * 0.391448974609375f);
-	color.z = (Y - 1.081668853759765625f) + (Cr * 2.017822265625f);
+	float Y = TexY.Sample(Sampler0, inputs.TEXCOORD.xy).x;
+	float Cb = TexCb.Sample(Sampler0, inputs.TEXCOORD.xy).x;
+	float Cr = TexCr.Sample(Sampler0, inputs.TEXCOORD.xy).x;
 
-	// Clamp for safety of pow as the above matrix produces negative values,
+	float3 color;
+	// usually in YCbCr the ranges are (in float):
+	// Y:   0.0-1.0
+	// Cb: -0.5-0.5
+	// Cr: -0.5-0.5
+	// but since this is a digital signal (in unsinged 8bit: 0-255) it's now:
+	// Y:  0.0-1.0
+	// Cb: 0.0-1.0
+	// Cr: 0.0-1.0
+	// the formula adjusts for that but was for BT.601 limited range while the video is definitely BT.709 full range
+	// matrix paramters have been adjusted for BT.709 full range
+	color.r = Y - 0.790487825870513916015625f + (Cr * 1.574800014495849609375f);
+	color.g = Y + 0.329009473323822021484375f - (Cb * 0.18732427060604095458984375f) - (Cr * 0.46812427043914794921875f);
+	color.b = Y - 0.931438446044921875f       + (Cb * 1.85559999942779541015625f);
+
+	// Clamp for safety as YCbCr<->RGB is not 100% accurate in float and can produce negative/invalid colors,
 	// this breaks the UI pass if we are using R16G16B16A16F textures,
 	// as UI blending produces invalid pixels if it's blended with an invalid color.
 	color = max(color, 0.f);
-#endif
 
 #if ENABLE_HDR
 #if SDR_USE_GAMMA_2_2
