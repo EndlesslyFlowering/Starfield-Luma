f5eb630be696c01024d2e169e0e2f75d7ae71cd0
Filoppi <filippotarpini@hotmail.it>
2023-10-10 20:44:20 +0300

    Forgotten root signatures

--- shaders/FilmGrain/RootSignature.hlsl
+++ shaders/FilmGrain/RootSignature.hlsl
@@ -7,5 +7,5 @@
 		"SRV(t0, space=8) " \
 	"), " \
 	\
-	"RootConstants(num32BitConstants=17, b3), " \
+	"RootConstants(num32BitConstants=19, b3), " \
 	"StaticSampler(s0, filter=FILTER_MIN_MAG_MIP_POINT, addressU=TEXTURE_ADDRESS_CLAMP, addressV=TEXTURE_ADDRESS_CLAMP, addressW=TEXTURE_ADDRESS_CLAMP, maxAnisotropy=1, comparisonFunc=COMPARISON_NEVER, space=8) "
