ff671607fe28836701cdf33505f3ff1036284c32
Filoppi <filippotarpini@hotmail.it>
2023-10-10 12:07:43 +0300

    Rename variables

--- shaders/HDRComposite/HDRComposite_ps.hlsl
+++ shaders/HDRComposite/HDRComposite_ps.hlsl
@@ -875,9 +875,9 @@ PSOutput PS(PSInput psInput)
 #endif // ENABLE_LUT
 
 #if GAMMA_CORRECT_SDR_RANGE_ONLY && (FIX_WRONG_SRGB_GAMMA_FORMULA || SDR_USE_GAMMA_2_2)
-	const float3 tonemappedPostProcessedGradedColorSDRColors = saturate(tonemappedPostProcessedGradedColor);
-	const float3 tonemappedPostProcessedGradedColorHDRColors = tonemappedPostProcessedGradedColor - tonemappedPostProcessedGradedColorSDRColors;
-	tonemappedPostProcessedGradedColor = tonemappedPostProcessedGradedColorSDRColors;
+	const float3 tonemappedPostProcessedGradedSDRColors = saturate(tonemappedPostProcessedGradedColor);
+	const float3 tonemappedPostProcessedGradedSDRExcessColors = tonemappedPostProcessedGradedColor - tonemappedPostProcessedGradedSDRColors;
+	tonemappedPostProcessedGradedColor = tonemappedPostProcessedGradedSDRColors;
 #endif
 // Do these even if "ENABLE_LUT" is false, for consistency
 #if FIX_WRONG_SRGB_GAMMA_FORMULA && 0 // Disabled as this looks awful, probably because the Bethesda optimized sRGB function doesn't even stay in the 0-1 range so there's no way to recover from it
@@ -889,7 +889,7 @@ PSOutput PS(PSInput psInput)
 	tonemappedPostProcessedGradedColor = pow(gamma_linear_to_sRGB(tonemappedPostProcessedGradedColor), 2.2f);
 #endif // SDR_USE_GAMMA_2_2
 #if GAMMA_CORRECT_SDR_RANGE_ONLY && (FIX_WRONG_SRGB_GAMMA_FORMULA || SDR_USE_GAMMA_2_2)
-	tonemappedPostProcessedGradedColor += tonemappedPostProcessedGradedColorHDRColors;
+	tonemappedPostProcessedGradedColor += tonemappedPostProcessedGradedSDRExcessColors;
 #endif // GAMMA_CORRECT_SDR_RANGE_ONLY
 
 #endif // APPLY_MERGED_COLOR_GRADING_LUT
