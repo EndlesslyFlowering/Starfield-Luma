b9b9462d9b3fc99b89b87ecfb4b9b21c28e3aa95
Filoppi <filippotarpini@hotmail.it>
2023-10-10 21:11:02 +0300

    Fix

--- Plugin/src/Settings.h
+++ Plugin/src/Settings.h
@@ -95,7 +95,7 @@ namespace Settings
 		float    DevSetting04;
 		float    DevSetting05;
 	};
-	static inline uint32_t shaderConstantsSize = 17;
+	static inline uint32_t shaderConstantsSize = 19;
 
     class Main : public DKUtil::model::Singleton<Main>
     {
