0395567b62400dc2ce315b8161fb14a27a5d69c0
Filoppi <filippotarpini@hotmail.it>
2023-11-23 03:37:24 +0200

    Handle rare case in the restore post process func, where the "ColorToPostProcess" was zero, so we couldn't restore the target color through a multiplication

--- shaders/HDRComposite/HDRComposite_ps.hlsl
+++ shaders/HDRComposite/HDRComposite_ps.hlsl
@@ -725,7 +725,7 @@ float3 RestorePostProcess(float3 ColorToPostProcess, float3 SourceColor, float3 
 // Note: in case "INVERT_TONEMAP_TYPE" was 0, we might want to test the "postProcessedOffsetColor" blend in range more carefully.
 // For the "INVERT_TONEMAP_TYPE" >0 case, this seems to work great, with the "MaxShadowsColor" setting that is there, anything more will raise colors.
 #if 1
-	float3 newPostProcessedColor = lerp(postProcessedOffsetColor, postProcessedRatioColor, saturate(SourceColor / MaxShadowsColor));
+	float3 newPostProcessedColor = lerp(postProcessedOffsetColor, postProcessedRatioColor, max(saturate(ColorToPostProcess / MaxShadowsColor), saturate(SourceColor / MaxShadowsColor)));
 #else // Doing the branching this way might not be so good, as near black colors could still end up with crazy value due to divisions between tiny values. Might not work with "HDR_TONEMAP_TYPE" > 1 (tonemap by luminance)
 	float3 newPostProcessedColor = select(PostProcessedColor == 0.f, postProcessedOffsetColor, postProcessedRatioColor);
 #endif
