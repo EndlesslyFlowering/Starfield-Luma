baca447ef9c28e44bb4baa2d2f5ae69c070adc6f
Filoppi <filippotarpini@hotmail.it>
2024-05-07 02:43:43 +0300

    Change default HDR paper white to 203 nits (from 200) and make the settings slider step from 10 to 1 (more annoying for quickly adjustments, but better for fine adjustments)

--- Plugin/src/Settings.h
+++ Plugin/src/Settings.h
@@ -248,24 +248,24 @@ namespace Settings
 				"\nFor a proper experience, adjust this to your liking, avoiding going too close to the peak brightness,"
 				" as higher values will actually reduce contrast (and thus dynamic range)."
 				"\n"
-				"\nThe default value is 200.",
+				"\nThe default value is 203.",
 			"GamePaperWhite", "HDR",
-			200, /*ITU reference default is 203 but we don't want to confuse users*/
+			203,
 			80,
 			500,
-			10
+			1
 		};
 		ValueStepper UIPaperWhite{
 			SettingID::kHDR_UIPaperWhite,
 			"UI Paper White",
 			"Sets the user interface (UI) brightness in HDR modes."
 				"\n"
-				"\nThe default value is 200",
+				"\nThe default value is 203",
 			"UIPaperWhite", "HDR",
-			200, /*ITU reference default is 203 but we don't want to confuse users*/
+			203,
 			80,
 			500,
-			10
+			1
 		};
 		Slider ExtendGamut{
 			SettingID::kHDR_ExtendGamut,
