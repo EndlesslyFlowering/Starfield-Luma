c48bcd912ea675c1046fa1cc733d03412e31c27d
Nukem <Nukem@outlook.com>
2025-03-15 07:59:00 -0400

    Fix shader root signature constant sizes

--- shaders/shared.hlsl
+++ shaders/shared.hlsl
@@ -91,6 +91,6 @@ struct StructHdrDllPluginConstants
 	float DevSetting05; // 0-1 variable for development. Default 0.5
 };
 
-#define HDR_PLUGIN_CONSTANTS_SIZE "26"
+#define HDR_PLUGIN_CONSTANTS_SIZE "27"
 
 ConstantBuffer<StructHdrDllPluginConstants> HdrDllPluginConstants : register(b3, space0);
